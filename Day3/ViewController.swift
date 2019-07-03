801af08c390c37f88e4f6dd00bdd36c21c404798
Nikhil Nikhil <nikhilthakur050@gmail.com>
2019-07-02 20:09:15 -0400

    slider with label

--- Day3/ViewController.swift
+++ Day3/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgjungle: UIImageView!
+    
+    @IBOutlet weak var switchoutlet: UISwitch!
+    @IBOutlet weak var lblSlider: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+       // switchoutlet.isOn = false
         // Do any additional setup after loading the view.
     }
-
-
+   
+    @IBAction func switchchanger(_ sender: UISwitch) {
+        if sender.isOn
+        {
+            imgjungle.image = UIImage(named: "home")
+        }
+        else{
+           imgjungle.image = UIImage(named: "gorilla")
+        }
+    }
+    
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        lblSlider.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func btnSetImageClick(_ sender: UIButton) {
+        imgjungle.image = UIImage(named: "elephant.png")
+        
+       //view.backgroundColor = UIColor(red: 100, green: 50, blue: 0)
+    }
+    
 }
 
